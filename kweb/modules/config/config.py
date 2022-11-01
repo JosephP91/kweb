f5767bab9bcda1028aee0d197e9be18324b96e61
Giuseppe Persico <giuseppe.persico91@gmail.com>
2022-11-01 15:42:09 +0100

    Fixed indentation

--- kweb/modules/config/config.py
+++ kweb/modules/config/config.py
@@ -5,10 +5,10 @@ from munch import DefaultMunch
 
 
 class ConfigReader:
-	@staticmethod
-	def read(env_name: str) -> DefaultMunch:
-		cur_abs_path = os.path.abspath(os.path.dirname(__file__))
-		full_file_path = os.path.join(cur_abs_path, "../../config/application.yml")
-		with open(full_file_path) as config_file_stream:
-			yaml_content = yaml.safe_load(config_file_stream)[env_name]
-			return DefaultMunch.fromDict(yaml_content)
+    @staticmethod
+    def read(env_name: str) -> DefaultMunch:
+        cur_abs_path = os.path.abspath(os.path.dirname(__file__))
+        full_file_path = os.path.join(cur_abs_path, "../../config/application.yml")
+        with open(full_file_path) as config_file_stream:
+            yaml_content = yaml.safe_load(config_file_stream)[env_name]
+            return DefaultMunch.fromDict(yaml_content)
